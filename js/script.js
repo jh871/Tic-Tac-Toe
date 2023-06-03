fd9d41707381b9e87de5cb00da5302297edc4ea5
jh871 <jennyh871@gmail.com>
2023-06-03 20:27:57 +0100

    added starter logic incl event listeners

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Grab all elements
+// add event listeners to squares - done
+// identify square by ID of event target - done
+
+//grabbing all squares
+const squares = document.querySelectorAll(".box");
+
+//arrays to hold each square
+let emptySquares = [];
+let userSquares = [];
+let oppSquares = [];
+
+
+
+//adding event listener to each square
+squares.forEach((square) => square.addEventListener('click', printHi));
+
+// array to hold all squares
+squares.forEach((square) => emptySquares.push(square.id))
+// console.log(emptySquares);
+
+//literally just prints hi to each square right now
+//also bumps each clicked square from emptySq array into userSq array
+function printHi(e){
+    let box = e.target;
+    console.log(box.id);
+    emptySquares = emptySquares.filter(x => x !== box.id)
+    userSquares.push(box.id)
+    console.log(emptySquares);
+    console.log(userSquares);
+    box.innerHTML = "<p>Hi</p>";
+    
+}
+
+
+
+/* computer's go for vs computer mode
+function compGo(){
+    let choice = emptySquares.length;
+    let number = Math.floor(Math.random()*choice);
+    console.log(number);
+    let number = Math.floor(Math.random()*choice);
+    let computerGo = emptySquares[number];
+}
+*/
+
+//Alternate goes between two people
+//draw different shape
+//or initially add different bg colours to each square using addAttribute - style
+//win conditions
+//update score/info
+
