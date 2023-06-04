4bb8feef03b4d59ec7e8bb6b8bd40bb8332515f1
jh871 <jennyh871@gmail.com>
2023-06-04 12:40:27 +0100

    player alt each go, eventlistener removed after selection

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 // Grab all elements
 // add event listeners to squares - done
 // identify square by ID of event target - done
-
+//set turn
+let player = "One";
 //grabbing all squares
 const squares = document.querySelectorAll(".box");
+//info
+const info = document.getElementById("info")
+//update text
+info.textContent = "Player " + player + "'s turn";
 
 //arrays to hold each square
 let emptySquares = [];
 let userSquares = [];
 let oppSquares = [];
 
-//set turn
-let player = "playerOne";
+
 
 
 //adding event listener to each square
@@ -28,17 +32,24 @@ function printHi(e){
     console.log(box.id);
     emptySquares = emptySquares.filter(x => x !== box.id)
     userSquares.push(box.id)
-    console.log(emptySquares);
-    console.log(userSquares);
+    // console.log(emptySquares);
+    // console.log(userSquares);
     let go = document.getElementById(box.id);
 
-    go.classList.add(player);
-
-    //Alternate goes between two people
+    go.classList.add(player); //only works once
     
-    if (player !== "playerTwo") {
-        player = "playerTwo";
-    }
+    
+
+    //Alternate goes between two people -
+    
+    if (player === "One") {
+        player = "Two";
+    } else {
+        player = "One";
+    };
+    //update text
+    info.textContent = "Player " + player + "'s turn";
+    go.removeEventListener('click');
 }
 
 
@@ -56,7 +67,46 @@ function compGo(){
 
 
 //draw different shape
-//or initially add different bg colours to each square using addAttribute - style
+//or initially add different bg colours - Done
 //win conditions
 //update score/info
 
+/*Overall game:
+-----------------------
+Initialise variables:
+//arrays to hold squares
+let emptySquares = [];
+let userSquares = [];
+let oppSquares = [];
+
+//set turn
+let player = "playerOne";
+
+//win conditions:
+
+
+
+
+
+---------------
+Grab elements:
+//game area/all squares
+const squares = document.querySelectorAll(".box");
+//info
+const info = document.getElementById("#info")
+
+
+Start game:
+//update text
+info.textContent = `$(player) take your turn`;
+
+Turn one: 
+click square, 
+change square, 
+check for win
+switch to other player
+
+Turn two: same again
+
+reset board
+*/
