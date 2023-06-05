e642895252bb87a2e39a494cfd36290ac413cd60
jh871 <jennyh871@gmail.com>
2023-06-05 13:45:16 +0100

    tidied code

--- js/script.js
+++ js/script.js
@@ -1,46 +1,38 @@
-// Grab all elements
-// add event listeners to squares - done
-// identify square by ID of event target - done
-
-//grabbing all squares
+// Grab elements
+//squares
 const squares = document.querySelectorAll(".box");
+
 //info
 const info = document.getElementById("info")
 
-
-//arrays to hold each square
+// Initialise variables:
+//arrays to hold squares
 let emptySquares = [];
-let oneSquares = [];
-let twoSquares = [];
+squares.forEach((square) => emptySquares.push(square.id));
+
 //set turn
 let player = "One";
 
-//update text
+//set text
 info.textContent = "Player " + player + "'s turn";
 
+
 //adding event listener to each square
-squares.forEach((square) => square.addEventListener('click', printHi));
+squares.forEach((square) => square.addEventListener('click', takeTurn));
 
-// array to hold all squares
-squares.forEach((square) => emptySquares.push(square.id))
-// console.log(emptySquares);
 
-//literally just prints hi to each square right now
-//also bumps each clicked square from emptySq array into userSq array
-function printHi(e){
-    let box = e.target;
-    console.log(box.id);
-    emptySquares = emptySquares.filter(x => x !== box.id)
-    oneSquares.push(box.id)
-
-    let go = document.getElementById(box.id);
-
-    go.classList.add(player); //only works once
+function takeTurn(event){
+    let box = event.target;
+    //remove square from playable squares
+    emptySquares = emptySquares.filter(x => x !== box.id);
     
-    checkWin();
+    let go = document.getElementById(box.id);
+    //modify square for player's turn
+    go.classList.add(player); 
+    
+    // checkWin();
 
     //Alternate goes between two people -
-    
     if (player === "One") {
         player = "Two";
     } else {
@@ -48,54 +40,82 @@ function printHi(e){
     };
     //update text
     info.textContent = "Player " + player + "'s turn";
-    go.removeEventListener('click', printHi);
+    go.removeEventListener('click', takeTurn);
 }
+
 
 //win conditions:
 function checkWin() {
     console.log(squares)
-
-    const winConds = [
-        [0, 1, 2],
+    const winConditions = [
+        [0, 1, 3],
         [0, 4, 8],
-        [0, 3, 7],
+        [0, 3, 6],
         [1, 4, 7],
-        [2, 5, 8],
-        [2, 4, 7],
+        [3, 5, 8],
+        [3, 4, 6],
         [3, 4, 5],
         [6, 7, 8],
-    ]
+    ]    
+    //Attempt method 1:
+    //loop through arrays
+    //compare class
+    // Check player One win
 
-    //Check player One win
-    winConds.forEach(arr => { 
-        //takes each winConds array and checks if squares with corresponding IDs have the class 'One' (or 'Two') in them. If all 3 squares do, it's a win.
-        let winnerOne = arr.every(square => squares[square].classList.contains("One"));
-
-        if (winnerOne) {
-        //update text
-        info.textContent = "Player One wins!";
-        return;
-        }
-    })
-
-
-    // //Check player Two win
-    // winConds.forEach(arr => {
-    //     let winnerTwo = arr.every(id => squares[id].classList.contains("Two"))
-
-    //     if (winnerTwo) {
-    //     //update text
-    //     info.textContent = "Player Two wins!";
-    //     return;
-    //     }
-    // })
-
-    //currently doesn't like (squares[square])
+    if (winnerOne) {
+    //update text
+    info.textContent = "Player One wins!";
+    return;
+    }
+    
+    //Check player Two win
+    if (winnerTwo) {
+    //update text
+    info.textContent = "Player Two wins!";
+    return;
+    }
 }
 
 
+// draw condition
+        // if (emptySquares === [] && winConds = false) {
+        //     info.textContent("It's a draw!")
+        // } else {
+        //     ()
+        // }
+
+// ---------------
 
 
+// Turn one: 
+// click square, 
+// change square, 
+// check for win
+// switch to other player
+
+// Turn two: same again
+
+// reset board
+//#########################################
+//Notes: 
+
+//win conditions - different versions:
+/* Method 1: cycle through arrays of win conditions checking for matching class attribute on box
+*/
+
+/* Method two: 
+push each clicked square to an array for each player and then check if their array includes a winCond array
+//user played squares - method 2
+let userSquares = [];
+let oppSquares = [];
+*/
+
+
+/* Method 3: 
+Use data-value attributes on squares where each winning line adds up to 15
+// let value = box.getAttribute('data-value');
+    // console.log(value);
+*/
 
 /* computer's go for vs computer mode
 function compGo(){
@@ -105,67 +125,4 @@ function compGo(){
     let number = Math.floor(Math.random()*choice);
     let computerGo = emptySquares[number];
 }
-*/
-
-
-
-
-//win conditions
-
-
-/*Overall game:
------------------------
-Initialise variables:
-//arrays to hold squares
-let emptySquares = [];
-let userSquares = [];
-let oppSquares = [];
-
-//set turn
-let player = "playerOne";
-
-//win conditions:
-function checkWin() {
-    console.log(squares)
-
-    const winConds = [
-        [0, 1, 3],
-        [0, 4, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [3, 5, 8],
-        [3, 4, 6],
-        [3, 4, 5],
-        [6, 7, 8],
-    ]
-}
-
-// draw condition
-        if (emptySquares === [] && winConds = false) {
-            info.textContent("It's a draw!")
-        } else {
-            ()
-        }
-
----------------
-Grab elements:
-//game area/all squares
-const squares = document.querySelectorAll(".box");
-//info
-const info = document.getElementById("#info")
-
-
-Start game:
-//update text
-info.textContent = `$(player) take your turn`;
-
-Turn one: 
-click square, 
-change square, 
-check for win
-switch to other player
-
-Turn two: same again
-
-reset board
 */
